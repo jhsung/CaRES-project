703562a072c75b22da6361fc2925cf74dddf9977
jhsung <jhsung.cal@gmail.com>
2014-07-09 12:07:37 -0500

    Trying to get our data

--- funwithjs.js
+++ funwithjs.js
@@ -1,3 +1,10 @@
+var evilGlobal;
 var hello = function (name) {
 	console.log("Hello " + name);
+}
+var loadData = function(filename) {
+	$.get("data/"+filename,function(res) {
+		evilGlobal = res;
+		console.log("All Done");
+	});
 }
